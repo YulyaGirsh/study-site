201148938b0b4af4a8a3b538835e1d783f89c78b
123 <123@mail.ru>
2022-05-31 15:56:32 +0500

    Создание второй модели + связь моделей

--- Django/touristsite/touristsite/places/admin.py
+++ Django/touristsite/touristsite/places/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
-from .models import Places
+from .models import Places, Categories
 
 
 # Register your models here.
 
 
 class PlacesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title',  'creates_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'creates_at','category',  'updated_at', 'is_published')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
 
 
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
 admin.site.register(Places, PlacesAdmin)
+
+admin.site.register(Categories, CategoriesAdmin)
--- Django/touristsite/touristsite/places/models.py
+++ Django/touristsite/touristsite/places/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 
 class Places(models.Model):
-    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Текст')  # поле не обязательно к заполнению
     creates_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-
-    # category = models.ForeignKey('Categories', on_delete=models.PROTECT, null=True, verbose_name='Тип локации')
+    category = models.ForeignKey('Categories', on_delete=models.PROTECT, null=True, verbose_name='Тип локации')
 
     def __str__(self):
         return self.title
@@ -17,4 +16,16 @@ class Places(models.Model):
     class Meta:
         verbose_name = 'Место'
         verbose_name_plural = 'Места'
-        ordering = ['-creates_at', 'title']
+        ordering = ['-creates_at']
+
+
+class Categories(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование', db_index=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
