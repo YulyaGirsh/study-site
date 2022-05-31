c9ea223ac356ae41de3acb2c94a3bfc1ba6b9e31
123 <123@mail.ru>
2022-05-31 12:30:16 +0500

    Подключение шаблонов

--- Django/touristsite/touristsite/places/views.py
+++ Django/touristsite/touristsite/places/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Places
 
 
 def index(request):
-    return HttpResponse('Всем приветик')
+    places = Places.objects.all()
+    return HttpResponse('Hello')
+    # return render(request, 'places.index', {'places':places, 'title': 'Список направлений'})
