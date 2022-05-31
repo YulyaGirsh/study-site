eb81c68227f2c7969dd49adde0a4dc759f7836ad
123 <123@mail.ru>
2022-05-31 18:42:53 +0500

    настройка url для категорий

--- Django/touristsite/touristsite/places/views.py
+++ Django/touristsite/touristsite/places/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Places
+from .models import Places, Categories
 
 
 def index(request):
     places = Places.objects.all()
-    return render(request, 'places/index.html', {'places': places, 'title': 'Список направлений'})
+    categories = Categories.objects.all()
+    return render(request, 'places/index.html', {'places': places, 'title': 'Список направлений', 'category' : categories})
+
+
+def get_category(request, category_id):
+    places = Places.objects.filter(category_id=category_id)
+    categories = Categories.objects.all()
+    category = Categories.objects.get(pk=category_id)
+    return render(request,  'places/category.html', {'places': places, 'category': category, 'categories': categories})
