18202acf2c99c6f79713aa482232eb9d1fac0f42
123 <123@mail.ru>
2022-06-20 21:59:09 +0500

    добавлены классы DetailView и ListView

--- Django/touristsite/touristsite/places/urls.py
+++ Django/touristsite/touristsite/places/urls.py
@@ -4,7 +4,9 @@ from .views import *
 urlpatterns = [
     # path('', index, name='home'),
     path('', HomePlaces.as_view(), name='home'),
-    path('category/<int:category_id>/', get_category, name='category'),
+    # path('category/<int:category_id>/', get_category, name='category'),
+    path('category/<int:category_id>/', PlacesByCategory.as_view(), name='category'),
+    path('places/<int:places_id>/', ViewPlace.as_view(), name='view_place'),
     path('contact/', contact, name='contact'),
     path('sales/', sales, name='sales'),
     path('prices/', prices, name='prices'),
--- Django/touristsite/touristsite/places/views.py
+++ Django/touristsite/touristsite/places/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_list_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Places, Categories, Sales, Review
 from .forms import ReviewForm2
 
@@ -8,25 +8,60 @@ class HomePlaces(ListView):
     model = Places
     template_name = 'places/index.html'
     context_object_name = 'places'
-    extra_context = {'title': 'Главная'}
-
-
-def index(request):
-    places = Places.objects.all()
     categories = Categories.objects.all()
-    return render(request, 'places/index.html',
-                  {'places': places, 'title': 'Список направлений', 'category': categories})
+    extra_context = {'title': 'Главная', 'category': categories}
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница сайта'
+        return context
+
+    def get_queryset(self):
+        return Places.objects.filter(is_published=True)
 
 
-def get_category(request, category_id):
-    places = Places.objects.filter(category_id=category_id)
+# def index(request):
+#     places = Places.objects.all()
+#     categories = Categories.objects.all()
+#     return render(request, 'places/index.html',
+#                   {'places': places, 'title': 'Список направлений', 'category': categories})
+
+
+class PlacesByCategory(ListView):
+    model = Places
+    template_name = 'places/category.html'
+    context_object_name = 'places'
     categories = Categories.objects.all()
-    category = Categories.objects.get(pk=category_id)
-    return render(request, 'places/category.html', {'places': places, 'category': category, 'categories': categories})
+    extra_context = {'category': categories, 'categories': categories}
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Categories.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return Places.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
+
+class ViewPlace(DetailView):
+    model = Places
+    # template_name = 'places/places_detail.html'
+    pk_url_kwarg = 'places_id'
+    categories = Categories.objects.all()
+    context_object_name = 'item'
+    extra_context = {'category': categories, 'categories': categories}
+
+
+# def get_category(request, category_id):
+#     places = Places.objects.filter(category_id=category_id)
+#     categories = Categories.objects.all()
+#     category = Categories.objects.get(pk=category_id)
+#     return render(request, 'places/category.html', {'places': places, 'category': category, 'categories': categories})
 
 
 def contact(request):
-    return render(request, 'places/contact.html')
+    return render(request, 'places/contact.html', {'title': 'Контакты'})
 
 
 def sales(request):
